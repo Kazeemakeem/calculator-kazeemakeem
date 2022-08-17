e82f207d21301daa7400ceb4a29a3decd57b4342
akeemkazeem <kazeemakeem69@gmail.com>
2022-08-17 07:44:14 +0100

    updating files

--- src/components/screen/Screen.tsx
+++ src/components/screen/Screen.tsx
@@ -17,7 +17,7 @@ const Screen = () => {
   const result = ans.split("").includes('e') ? resultArr[0].slice(0,12) + 'e' + resultArr[1] : ans
 
   return (
-    <div className={`${theme === 0 ? 'bg-[#181f32]' : ''}${theme === 1 ? 'bg-white' : ''}${theme === 2 ? 'bg-[#1e0836]' : ''} max-h-[10rem] w-[40rem] mx-auto px-10 rounded-2xl flex flex-col justify-center gap-2 pb-6`}>
+    <div className={`${theme === 0 ? 'bg-[#181f32]' : ''}${theme === 1 ? 'bg-white' : ''}${theme === 2 ? 'bg-[#1e0836]' : ''} h-[10rem] w-[40rem] mx-auto px-10 rounded-2xl flex flex-col justify-center gap-2 pb-6`}>
       <p className={`text-3xl ${theme === 0 ? 'text-white' : ''} ${theme === 1 ? 'text-gray-700' : ''} ${theme === 2 ? 'text-[#fdee45]' : ''} font-[400] flex justify-end items-center text-ellipsis overflow-hidden`}>{expr}_</p>
       <p className={`text-6xl ${theme === 0 ? 'text-white' : ''} ${theme === 1 ? 'text-gray-700' : ''} ${theme === 2 ? 'text-[#fdee45]' : ''} font-bold flex justify-end items-center text-ellipsis overflow-hidden`}>{result}</p>
     </div>
