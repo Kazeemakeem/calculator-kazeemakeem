8bd460d1558616a11463c02a069f0177a7dee220
akeemkazeem <kazeemakeem69@gmail.com>
2022-08-17 07:48:12 +0100

    updating files

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const dispatch = useAppDispatch()
   const theme = useAppSelector(state => state.header.theme) % 3
   return (
-    <div className={`App h-screen ${theme === 0 ? 'bg-[#3b4664]' : ''} ${theme === 1 ? 'bg-[#e6e6e6]' : ''} ${theme === 2 ? 'bg-[#17062a]' : ''}  min-w-[40rem] flex flex-col gap-8 justify-center items-center px-3 sm:px-0 mx-auto object-cover`}>
+    <div className={`App h-screen ${theme === 0 ? 'bg-[#3b4664]' : ''} ${theme === 1 ? 'bg-[#e6e6e6]' : ''} ${theme === 2 ? 'bg-[#17062a]' : ''}  min-w-[40rem] flex flex-col gap-8 justify-center items-center px-3 sm:px-0 mx-auto object-fill`}>
       <HeaderView />
       <Screen />
       <Keyboard />
